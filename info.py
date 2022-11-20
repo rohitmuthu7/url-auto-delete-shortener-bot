50c827579e53f393ccea2dc960305b72b2182231
rohitmuthu7 <99496913+rohitmuthu7@users.noreply.github.com>
2022-11-20 19:42:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/How_to_Download_From_Search_Bot/2"
+DOWNLOAD_TEXT_URL = "https://t.me/gm_download/76"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
