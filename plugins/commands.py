7127cc9606fc48beae1d10bc7c9b3d40ea1d3300
rohitmuthu7 <99496913+rohitmuthu7@users.noreply.github.com>
2023-02-22 17:48:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('GROUP', url='https://t.me/movie_request_tami') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('GROUP', url='https://t.me/New_tamil_movier') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
